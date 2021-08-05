0348a6776693ac1a99e0eaafa5d1c85fa43e95c7
mjesar <mohammadalijaisar@gmail.com>
2021-08-05 21:03:22 +0500

    add test permit params for user inputs

--- app/controllers/api/v1/tests_controller.rb
+++ app/controllers/api/v1/tests_controller.rb
@@ -1,5 +1,11 @@
 class Api::V1::TestsController < ApplicationController
   def create
-  	render json: "OK"
+  	render json: 'OK'
+  end
+
+  private
+
+  def test_params
+    params.require(:test).permit(:url, :max_ttfb, :max_tti, :max_speed_index, :max_ttfp)
   end
 end
