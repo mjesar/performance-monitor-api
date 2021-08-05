70115301fe5c94c21565b8d83b286ca86ea9bb80
mjesar <mohammadalijaisar@gmail.com>
2021-08-05 22:05:17 +0500

    get actual data in methods

--- app/controllers/api/v1/tests_controller.rb
+++ app/controllers/api/v1/tests_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::TestsController < ApplicationController
   def create
-  	render json: performance_monitor
+  	render json: ttfb
   end
 
   private
@@ -12,5 +12,21 @@ class Api::V1::TestsController < ApplicationController
   def performance_monitor
     response = HTTParty.get("https://www.googleapis.com/pagespeedonline/v5/runPagespeed?key=AIzaSyBXLkGtNEMKcXXjj1Kv-h8okmloFLM-h94&strategy=desktop&url=#{test_params['url']}")
     payload = ::JSON.parse(response.body)
+  end
+
+  def ttfb
+     performance_monitor['lighthouseResult']['audits']['first-contentful-paint']['numericValue']
+  end
+
+  def ttfp
+    performance_monitor['lighthouseResult']['audits']['first-meaningful-paint']['numericValue']
+  end
+
+  def tti
+     performance_monitor['lighthouseResult']['audits']['interactive']['numericValue']
+  end
+
+  def index_speed
+     payload['lighthouseResult']['audits']['speed-index']['numericValue']
   end
 end
