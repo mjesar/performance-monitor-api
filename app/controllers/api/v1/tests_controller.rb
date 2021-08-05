33999ee6496d44f673c6c90e7763ca1253af6b8f
mjesar <mohammadalijaisar@gmail.com>
2021-08-05 21:54:42 +0500

    get PageSpeed Insights API and convert it to json

--- app/controllers/api/v1/tests_controller.rb
+++ app/controllers/api/v1/tests_controller.rb
@@ -1,11 +1,16 @@
 class Api::V1::TestsController < ApplicationController
   def create
-  	render json: 'OK'
+  	render json: performance_monitor
   end
 
   private
 
   def test_params
     params.require(:test).permit(:url, :max_ttfb, :max_tti, :max_speed_index, :max_ttfp)
+  end
+
+  def performance_monitor
+    response = HTTParty.get("https://www.googleapis.com/pagespeedonline/v5/runPagespeed?key=AIzaSyBXLkGtNEMKcXXjj1Kv-h8okmloFLM-h94&strategy=desktop&url=#{test_params['url']}")
+    payload = ::JSON.parse(response.body)
   end
 end
