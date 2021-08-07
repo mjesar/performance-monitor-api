898c9d34c506f7d6af5b9ab8cc40cb2b5e0cc939
mjesar <mohammadalijaisar@gmail.com>
2021-08-07 16:20:52 +0500

    code refactor and make it look like according to requirments in documet

---
+++ app/controllers/api/tests_controller.rb
@@ -0,0 +1,25 @@
+class Api::TestsController < ApplicationController
+  
+  def create
+    speed_test_insights = speed_test_insight
+    test_data = Test.new(test_params.merge(speed_test_insights))
+    if test_data.save
+      
+      render json: speed_test_insights
+    else
+      render json: test_data.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def test_params
+    params.permit(:url, :max_ttfb, :max_tti, :max_speed_index, :max_ttfp)
+  end
+  
+  def speed_test_insight
+    SpeedTestInsight.new(test_params).call
+  rescue StandardError
+    nil
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   scope module: :api, defaults: { format: :json } do
-    namespace :v1 do
-    	resource :performance_monitor,controller:  :performance_monitor
-    	resources :tests
-    end
+    resources :tests
   end
 end
