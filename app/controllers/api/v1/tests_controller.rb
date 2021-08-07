b123ef335e141be3628cb6697924aea07f89313f
mjesar <mohammadalijaisar@gmail.com>
2021-08-07 15:24:56 +0500

    check if tests passed then render data and save in record

--- app/controllers/api/v1/tests_controller.rb
+++ app/controllers/api/v1/tests_controller.rb
@@ -1,9 +1,10 @@
 class Api::V1::TestsController < ApplicationController
   
   def create
-    test_data = Test.new(test_params.merge(lighthouse_results))
+    test_data = Test.new(test_params.merge(performance_monitor))
     if test_data.save
-      render json: test_data
+      
+      render json: performance_monitor 
     else
       render json: test_data.errors, status: :unprocessable_entity
     end
@@ -17,15 +18,32 @@ class Api::V1::TestsController < ApplicationController
 
   def performance_monitor
     response = HTTParty.get("https://www.googleapis.com/pagespeedonline/v5/runPagespeed?key=AIzaSyBXLkGtNEMKcXXjj1Kv-h8okmloFLM-h94&strategy=desktop&url=#{test_params['url']}")
-    ::JSON.parse(response.body)
+    payload = ::JSON.parse(response.body)
+    lighthouse_results(payload)
   end
 
-  def lighthouse_results
-    payload = performance_monitor    
-    ttfb = payload['lighthouseResult']['audits']['first-contentful-paint']['numericValue']
-    ttfp = payload['lighthouseResult']['audits']['first-meaningful-paint']['numericValue']
-    tti = payload['lighthouseResult']['audits']['interactive']['numericValue']
-    speed_index = payload['lighthouseResult']['audits']['speed-index']['numericValue']
-    { "ttfb" => ttfb, "ttfp" => ttfp, "tti" => tti, "speed_index" => speed_index}
+  def lighthouse_results(payload)    
+    ttfb = payload['lighthouseResult']['audits']['first-contentful-paint']['numericValue'].to_i
+    ttfp = payload['lighthouseResult']['audits']['first-meaningful-paint']['numericValue'].to_i
+    tti = payload['lighthouseResult']['audits']['interactive']['numericValue'].to_i
+    speed_index = payload['lighthouseResult']['audits']['speed-index']['numericValue'].to_i
+    payload_hash = { "ttfb" => ttfb, "ttfp" => ttfp, "tti" => tti, "speed_index" => speed_index}
+    test_pass = check_test?(payload_hash)
+    payload_hash.merge! passed: test_pass
+
+  end
+
+  def check_test?(payload_hash)
+    if payload_hash['ttfb'] > test_params['max_ttfb'].to_i
+      false
+    elsif payload_hash['ttfp'] > test_params['max_ttfp'].to_i
+      false 
+    elsif payload_hash['tti'] > test_params['max_tti'].to_i
+      false
+    elsif payload_hash['speed_index'] > test_params['max_speed_index'].to_i
+      false
+    else
+      true
+    end
   end
 end
