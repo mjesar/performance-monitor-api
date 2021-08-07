2b7cc121be048f639097af8fc83908ba7f6f549d
mjesar <mohammadalijaisar@gmail.com>
2021-08-07 16:05:53 +0500

    create a service to handle speed insight test code

--- app/controllers/api/v1/tests_controller.rb
+++ app/controllers/api/v1/tests_controller.rb
@@ -1,10 +1,11 @@
 class Api::V1::TestsController < ApplicationController
   
   def create
-    test_data = Test.new(test_params.merge(performance_monitor))
+    speed_test_insights = speed_test_insight
+    test_data = Test.new(test_params.merge(speed_test_insights))
     if test_data.save
       
-      render json: performance_monitor 
+      render json: speed_test_insights
     else
       render json: test_data.errors, status: :unprocessable_entity
     end
@@ -15,35 +16,40 @@ class Api::V1::TestsController < ApplicationController
   def test_params
     params.require(:test).permit(:url, :max_ttfb, :max_tti, :max_speed_index, :max_ttfp)
   end
-
-  def performance_monitor
-    response = HTTParty.get("https://www.googleapis.com/pagespeedonline/v5/runPagespeed?key=AIzaSyBXLkGtNEMKcXXjj1Kv-h8okmloFLM-h94&strategy=desktop&url=#{test_params['url']}")
-    payload = ::JSON.parse(response.body)
-    lighthouse_results(payload)
+  
+  def speed_test_insight
+    SpeedTestInsight.new(test_params).call
+  rescue StandardError
+    nil
   end
+  # def performance_monitor
+  #   response = HTTParty.get("https://www.googleapis.com/pagespeedonline/v5/runPagespeed?key=AIzaSyBXLkGtNEMKcXXjj1Kv-h8okmloFLM-h94&strategy=desktop&url=#{test_params['url']}")
+  #   payload = ::JSON.parse(response.body)
+  #   lighthouse_results(payload)
+  # end
 
-  def lighthouse_results(payload)    
-    ttfb = payload['lighthouseResult']['audits']['first-contentful-paint']['numericValue'].to_i
-    ttfp = payload['lighthouseResult']['audits']['first-meaningful-paint']['numericValue'].to_i
-    tti = payload['lighthouseResult']['audits']['interactive']['numericValue'].to_i
-    speed_index = payload['lighthouseResult']['audits']['speed-index']['numericValue'].to_i
-    payload_hash = { "ttfb" => ttfb, "ttfp" => ttfp, "tti" => tti, "speed_index" => speed_index}
-    test_pass = check_test?(payload_hash)
-    payload_hash.merge! passed: test_pass
+  # def lighthouse_results(payload)    
+  #   ttfb = payload['lighthouseResult']['audits']['first-contentful-paint']['numericValue'].to_i
+  #   ttfp = payload['lighthouseResult']['audits']['first-meaningful-paint']['numericValue'].to_i
+  #   tti = payload['lighthouseResult']['audits']['interactive']['numericValue'].to_i
+  #   speed_index = payload['lighthouseResult']['audits']['speed-index']['numericValue'].to_i
+  #   payload_hash = { "ttfb" => ttfb, "ttfp" => ttfp, "tti" => tti, "speed_index" => speed_index}
+  #   test_pass = check_test?(payload_hash)
+  #   payload_hash.merge! passed: test_pass
 
-  end
+  # end
 
-  def check_test?(payload_hash)
-    if payload_hash['ttfb'] > test_params['max_ttfb'].to_i
-      false
-    elsif payload_hash['ttfp'] > test_params['max_ttfp'].to_i
-      false 
-    elsif payload_hash['tti'] > test_params['max_tti'].to_i
-      false
-    elsif payload_hash['speed_index'] > test_params['max_speed_index'].to_i
-      false
-    else
-      true
-    end
-  end
+  # def check_test?(payload_hash)
+  #   if payload_hash['ttfb'] > test_params['max_ttfb'].to_i
+  #     false
+  #   elsif payload_hash['ttfp'] > test_params['max_ttfp'].to_i
+  #     false 
+  #   elsif payload_hash['tti'] > test_params['max_tti'].to_i
+  #     false
+  #   elsif payload_hash['speed_index'] > test_params['max_speed_index'].to_i
+  #     false
+  #   else
+  #     true
+  #   end
+  # end
 end
---
+++ app/services/speed_test_insight.rb
@@ -0,0 +1,43 @@
+class SpeedTestInsight
+
+  def initialize(params)
+    @params = params
+  end
+
+  def call
+	performance_monitor	
+  end
+
+  private
+
+  def performance_monitor
+    response = HTTParty.get("https://www.googleapis.com/pagespeedonline/v5/runPagespeed?key=AIzaSyBXLkGtNEMKcXXjj1Kv-h8okmloFLM-h94&strategy=desktop&url=#{@params['url']}")
+    payload = ::JSON.parse(response.body)
+    lighthouse_results(payload)
+  end
+
+  def lighthouse_results(payload)    
+    ttfb = payload['lighthouseResult']['audits']['first-contentful-paint']['numericValue'].to_i
+    ttfp = payload['lighthouseResult']['audits']['first-meaningful-paint']['numericValue'].to_i
+    tti = payload['lighthouseResult']['audits']['interactive']['numericValue'].to_i
+    speed_index = payload['lighthouseResult']['audits']['speed-index']['numericValue'].to_i
+    payload_hash = { "ttfb" => ttfb, "ttfp" => ttfp, "tti" => tti, "speed_index" => speed_index}
+    test_pass = check_test?(payload_hash)
+    payload_hash.merge! passed: test_pass
+
+  end
+
+  def check_test?(payload_hash)
+    if payload_hash['ttfb'] > @params['max_ttfb'].to_i
+      false
+    elsif payload_hash['ttfp'] > @params['max_ttfp'].to_i
+      false 
+    elsif payload_hash['tti'] > @params['max_tti'].to_i
+      false
+    elsif payload_hash['speed_index'] > @params['max_speed_index'].to_i
+      false
+    else
+      true
+    end
+  end
+end
