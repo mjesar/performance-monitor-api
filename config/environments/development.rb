5ea9feace9fc51b34c753c2d0f0e392114913897
mjesar <mohammadalijaisar@gmail.com>
2021-08-08 23:21:11 +0500

    add Rack::MethodOverride to avoid error while clear all emails in letter opener web

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,6 +36,7 @@ Rails.application.configure do
 
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
+  config.middleware.use Rack::MethodOverride
 
   config.action_mailer.delivery_method = :letter_opener_web
   config.action_mailer.perform_deliveries = true
