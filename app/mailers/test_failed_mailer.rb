7e4ff09390d06162cbef589e573eab94d79009f3
mjesar <mohammadalijaisar@gmail.com>
2021-08-08 23:12:18 +0500

    render all data from mailer

--- app/mailers/test_failed_mailer.rb
+++ app/mailers/test_failed_mailer.rb
@@ -1,6 +1,15 @@
 class TestFailedMailer < ApplicationMailer
   def test_fail(test)
-    @tests = test
+    @url = test['url']
+    @max_ttfb = test['max_ttfb']
+    @max_ttfp = test['max_ttfp']
+    @max_tti = test['max_tti']
+    @max_speed_index = test['max_speed_index']
+    @ttfb = test[:ttfb]
+    @ttfp = test[:ttfp]
+    @tti = test[:tti]
+    @speed_index = test[:speed_index]
+    @passed = test[:passed]
     mail(from: 'ali@example.com', to: 'ali@example.com', subject: 'welcome')
   end
 end
